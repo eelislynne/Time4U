40f35aba40d5e45b13a33e88f0619682982b88a7
Fini21 <eelis.lynne@gmail.com>
2017-10-15 14:16:25 +0300

    Styling + auto select

--- src/window.js
+++ src/window.js
@@ -1,11 +1,32 @@
-$("#currTime").text("Time: " + Math.floor(new Date().getTime()/1000));
 
+var currTime = $("#currTime");
 var input = $("#timeInput");
 var output = $("#timeOutput");
+
+currTime.text(Math.floor(new Date().getTime()/1000));
+
 input.on('input',function() {
 	var text = input.val();
 	output.text(convertTimestamp(parseInt(text)));
 });
+
+output.on('click', function() {
+	var select = document.getSelection();
+	var range = document.createRange();
+	range.selectNodeContents($(this)[0]);
+	select.removeAllRanges();
+	select.addRange(range);
+});
+
+currTime.on('click', function() {
+	var select = document.getSelection();
+	var range = document.createRange();
+	range.selectNodeContents($(this)[0]);
+	console.log(range);
+	select.removeAllRanges();
+	select.addRange(range);
+});
+
 
 function convertTimestamp(timestamp) {
   	var d = new Date(0);
